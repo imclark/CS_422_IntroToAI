c56c4cc0d6605a2910abc6fc2b0a9ba729b66a16
imclark <33160623+imclark@users.noreply.github.com>
2018-04-16 23:22:40 -0500

    finds area of a triangle with vertexes
    takes in a list : ((x1, y1) (x2, y2) (x3 ,y3))

---
+++ ianclark.lisp
@@ -0,0 +1,4 @@
+(defun answer-1 (L) 
+    " takes in the vetexes and goes through the given lists"
+    " May spit out a negative number depending on the verticies given"
+    (/ (- (* (- (first (cadar L)) (caaar L) ) (- (first (cdr (caddar x))) (caadar L))) (* (- (first (cadar L)) (caadar L)) (- (first (cdr (caddar L))) (caaar L)))) 2))
