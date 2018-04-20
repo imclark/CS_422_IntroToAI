fcf673df2e8755fec04ae1943bf5b9ef0bd8eb83
imclark <33160623+imclark@users.noreply.github.com>
2018-04-19 20:01:56 -0500

    Added test file

--- ianclark.lisp
+++ ianclark.lisp
@@ -2,3 +2,7 @@
     " takes in the vetexes and goes through the given lists"
     " May spit out a negative number depending on the verticies given"
     (/ (- (* (- (first (cadar L)) (caaar L) ) (- (first (cdr (caddar x))) (caadar L))) (* (- (first (cadar L)) (caadar L)) (- (first (cdr (caddar L))) (caaar L)))) 2))
+
+(defun andswer-2 (R)
+
+    )
---
+++ ianclark_test.lisp
@@ -0,0 +1,2 @@
+# creates the lsit of vertexes for answer 1
+(setq x (list ( (3 2) (1 4) (3 5))))
