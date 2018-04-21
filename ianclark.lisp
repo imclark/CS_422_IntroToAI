2ddbefea9233ed8700f39a0e3d194099530d06e3
imclark <33160623+imclark@users.noreply.github.com>
2018-04-21 16:24:53 -0500

    Created answer-3 and answer-3 test case

--- ianclark.lisp
+++ ianclark.lisp
@@ -3,11 +3,14 @@
     (abs (/ (- (* (- (first (cadr L)) (caar L) ) (- (first (last (caddr L))) (first (last (caddr L))))) (* (- (first (cdr (cadr L))) (first (last (caddr L)))) (- (first (caddr L)) (caar L)))) 2)))
 
 (defun answer-2 (R)
+    "if there is a fourth vertext then use the recursive call else use the normal list creator"
     (if (not (null (cdr (cddr R))))
         (and (setq next (append (list (car R)) (values (cddr R)))) 
         (append (list (list (car R) (car (cdr R)) (car (cddr R)))) ( answer-2 next)))
         (append (list (list (car R) (car (cdr R)) (car (cddr R)))))))
 
 (defun answer-3 (E)
-    (if (not (null E)) 
-        (and (setq trig ( answer-2 E)) ())))
+    "setup the return value, iterate through the list and sum the resulting areas from answer 1 then return the sum"
+    (setq ans 0)
+    (loop for x in E collect (setq ans (+ ans ( answer-1 x))))
+    (return-from answer-3 ans))
--- ianclark_test.lisp
+++ ianclark_test.lisp
@@ -4,10 +4,19 @@
 (answer-1 x)
 "output > 4"
 
+
+
 "Creates the list of sublists to pass into answer-2"
 (setq y '((1 2) (3 4) (5 6) (7 8) (9 10)))
 
 (answer-2 y)
-"output > (((1 2) (3 4) (5 6)) ((1 2) (5 6) (7 8)) ((1 2) (7 8) (9 10)))
+"output > (((1 2) (3 4) (5 6)) ((1 2) (5 6) (7 8)) ((1 2) (7 8) (9 10)))"
 
-"
+
+
+"setup for answer-3"
+(setq y '((1 2) (3 4) (5 6) (7 8) (9 10)))
+(setq triangle-list ( answer-2 y))
+
+( answer-3 triangle-list)
+"output > 18"
