433c7eec9d50f3fa19d8aea23e0ffdcf5af6d04f
imclark <33160623+imclark@users.noreply.github.com>
2018-04-21 01:13:44 -0500

    Second problem added
    and test cases

--- ianclark.lisp
+++ ianclark.lisp
@@ -3,6 +3,8 @@
     " May spit out a negative number depending on the verticies given"
     (/ (- (* (- (first (cadar L)) (caaar L) ) (- (first (cdr (caddar x))) (caadar L))) (* (- (first (cadar L)) (caadar L)) (- (first (cdr (caddar L))) (caaar L)))) 2))
 
-(defun andswer-2 (R)
-
-    )
+(defun answer-2 (R)
+    (if (not (null (cdr (cddr R))))
+        (and (setq next (append (list (car R)) (values (cddr R)))) 
+        (append (list (list (car R) (car (cdr R)) (car (cddr R)))) ( answer-2 next)))
+        (append (list (list (car R) (car (cdr R)) (car (cddr R))))) ))
