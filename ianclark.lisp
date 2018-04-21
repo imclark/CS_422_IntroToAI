2be9332d682d891576966d4150a658cfdc79aaac
imclark <33160623+imclark@users.noreply.github.com>
2018-04-21 15:12:40 -0500

    Corrected answer-1
    changed how it handles the input to take a '((x1 y1) (x2 y2) (x3 y3))
    
    made the answer always be a positive
    
    changed the test to match

--- ianclark.lisp
+++ ianclark.lisp
@@ -1,10 +1,13 @@
 (defun answer-1 (L) 
     " takes in the vetexes and goes through the given lists"
-    " May spit out a negative number depending on the verticies given"
-    (/ (- (* (- (first (cadar L)) (caaar L) ) (- (first (cdr (caddar x))) (caadar L))) (* (- (first (cadar L)) (caadar L)) (- (first (cdr (caddar L))) (caaar L)))) 2))
+    (abs (/ (- (* (- (first (cadr L)) (caar L) ) (- (first (last (caddr L))) (first (last (caddr L))))) (* (- (first (cdr (cadr L))) (first (last (caddr L)))) (- (first (caddr L)) (caar L)))) 2)))
 
 (defun answer-2 (R)
     (if (not (null (cdr (cddr R))))
         (and (setq next (append (list (car R)) (values (cddr R)))) 
         (append (list (list (car R) (car (cdr R)) (car (cddr R)))) ( answer-2 next)))
-        (append (list (list (car R) (car (cdr R)) (car (cddr R))))) ))
+        (append (list (list (car R) (car (cdr R)) (car (cddr R)))))))
+
+(defun answer-3 (E)
+    (if (not (null E)) 
+        (and (setq trig ( answer-2 E)) ())))
--- ianclark_test.lisp
+++ ianclark_test.lisp
@@ -1,8 +1,8 @@
 "creates the lsit of vertexes for answer 1"
-(setq x '( '(3 2) '(1 4) '(3 5)))
+(setq x '((1 2) (3 4) (5 6)))
 
 (answer-1 x)
-"output > -4"
+"output > 4"
 
 "Creates the list of sublists to pass into answer-2"
 (setq y '((1 2) (3 4) (5 6) (7 8) (9 10)))
